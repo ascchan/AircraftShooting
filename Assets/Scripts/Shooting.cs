509b0fdd69e325549cae44e19ca7ade8a790c6e0
Anthony Chan <achan@acm.org>
2026-09-07 19:52:44 -0400

    Added Sound for Artillery shooting

--- Assets/Scripts/Shooting.cs
+++ Assets/Scripts/Shooting.cs
@@ -5,23 +5,36 @@ public class Shooting : MonoBehaviour
     [SerializeField] private Rigidbody projectilePrefab;
     [SerializeField] private Transform weaponTip;
 
-    [SerializeField] private float launchForce = 40f;
-    [SerializeField] private float fireCooldown = 0.5f;
+    [SerializeField] private float launchForce;
+    [SerializeField] private float fireCooldown;
+    [SerializeField] private GameObject WeaponTipEffect;
+    [SerializeField] private float WeaponTipEffectLifeTime;
 
-    [SerializeField] private bool allowLeftMouseButton = true;
+    private float nextFireTime;
 
     private void Update()
     {
-        if ( Input.GetKeyDown(KeyCode.Space) )
+        if ( Input.GetKeyDown(KeyCode.Space) && Time.time >= nextFireTime )
         {
             FireProjectile();
+            nextFireTime = Time.time + fireCooldown;
         }
     }
 
     private void FireProjectile()
     {
         Rigidbody projectileInstance = Instantiate( projectilePrefab, weaponTip.position, weaponTip.rotation );
+        projectileInstance.AddForce( weaponTip.forward * launchForce, ForceMode.VelocityChange );
 
-        projectileInstance.AddForce( weaponTip.forward * launchForce, ForceMode.Impulse );
+        CreateWeaponTipEffect();
+    }
+
+    private void CreateWeaponTipEffect()
+    {
+        if (WeaponTipEffect != null)
+        {
+            GameObject effectInstance = Instantiate(WeaponTipEffect, weaponTip.position, weaponTip.rotation);
+            Destroy(effectInstance, WeaponTipEffectLifeTime);
+        }
     }
 }
